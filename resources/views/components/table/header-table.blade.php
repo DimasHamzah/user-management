7edda7d67873e2e7e35f5c5c50b61c54674e3482
dimas nurhamzah <hamzahdimas85@gmail.com>
2024-08-25 05:20:20 -0700

    create header and listview for usermanagement

---
+++ resources/views/components/table/header-table.blade.php
@@ -0,0 +1,8 @@
+<thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+    <tr>
+        <x-item.text-header :label="'Email'" />
+        <x-item.text-header :label="'UserName'" />
+        <x-item.text-header :label="'Created At'" />
+        <x-item.text-header :label="'Action'" />
+    </tr>
+</thead>
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,3 +1,31 @@
-<div>
-    <!-- Nothing worth having comes easy. - Theodore Roosevelt -->
-</div>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('User Management') }}
+        </h2>
+    </x-slot>
+
+    <section class="w-full md:max-w-2xl lg:max-w-4xl xl:max-w-7xl mx-auto px-4">
+        <div class="relative overflow-x-auto">
+            <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                <x-table.header-table />
+                <tbody>
+                    @foreach($users as $user)
+                        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                            <x-item.text-list-heading :label="$user->email" />
+                            <x-item.text-list :label="$user->name" />
+                            <x-item.text-list :label="$user->created_at" />
+                            <td class="px-6 py-4">
+                                <button type="button" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Red</button>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </section>
+
+    <section class="w-full md:max-w-2xl lg:max-w-4xl xl:max-w-7xl mx-auto px-4 py-4">
+        {{ $users->links() }}
+    </section>
+</x-app-layout>
