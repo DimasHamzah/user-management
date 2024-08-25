2b9f9a9db2c720021504818c93b9bb7e164320c0
dimas nurhamzah <hamzahdimas85@gmail.com>
2024-08-25 08:08:01 -0700

    change test queue

--- tests/Feature/Job/ProcessStoreBulkUserTest.php
+++ tests/Feature/Job/ProcessStoreBulkUserTest.php
@@ -3,8 +3,6 @@
 namespace Tests\Feature\Job;
 
 use App\Jobs\ProcessStoreBulkUser;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Facades\Queue;
 use Tests\TestCase;
 
