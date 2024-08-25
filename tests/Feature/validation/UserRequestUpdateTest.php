3ae6703fe06d445902da97ef7d5cfc70bf28a1b2
dimas nurhamzah <hamzahdimas85@gmail.com>
2024-08-25 04:43:52 -0700

    update test and request for userRequestUpdate

--- tests/Feature/validation/UserRequestUpdateTest.php
+++ tests/Feature/validation/UserRequestUpdateTest.php
@@ -2,19 +2,56 @@
 
 namespace Tests\Feature\validation;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
+use App\Http\Requests\UserRequestUpdate;
+use Illuminate\Support\Facades\Validator;
 use Tests\TestCase;
 
 class UserRequestUpdateTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
+    public function test_authorization()
     {
-        $response = $this->get('/');
+        $request = new UserRequestUpdate();
 
-        $response->assertStatus(200);
+        $this->assertTrue($request->authorize());
+    }
+
+    public function test_rules()
+    {
+        $request = new UserRequestUpdate();
+        $rules =  $request->rules();
+
+        $this->assertArrayHasKey('name', $rules);
+        $this->assertArrayHasKey('email', $rules);
+        $this->assertArrayHasKey('password', $rules);
+
+        $this->assertEquals('required|string', $rules['name']);
+        $this->assertEquals('nullable|email:rfc,dns|unique:users,email', $rules['email']);
+        $this->assertEquals('nullable|string', $rules['password']);
+    }
+
+    public function test_valid_user_request()
+    {
+        $data = [
+            'name' => 'testing',
+            'email' => 'testing@gmail.com',
+            'password' => '123456',
+        ];
+
+        $request = new UserRequestUpdate();
+        $request->replace($data);
+
+        $validator = Validator::make($request->all(), $request->rules());
+        $this->assertFalse($validator->fails());
+    }
+
+    public function test_invalid_user_request()
+    {
+        $data = [];
+
+        $request = new UserRequestUpdate();
+        $request->replace($data);
+
+        $validator = Validator::make($request->all(), $request->rules());
+        $this->assertTrue($validator->fails());
     }
 }
