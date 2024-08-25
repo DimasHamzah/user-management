a851438d23bac3eb7ff48eb63714f3270c313293
dimas nurhamzah <hamzahdimas85@gmail.com>
2024-08-25 07:42:44 -0700

    finish feature get all users

--- tests/Feature/Api/UserManagementControllerApiTest.php
+++ tests/Feature/Api/UserManagementControllerApiTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Api;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -14,9 +15,18 @@ class UserManagementControllerApiTest extends TestCase
             'Accept' => 'application/json',
             'Authorization' => ''
         ])->get('api/users');
-        $response->assertStatus(422);
+        $response->assertStatus(401);
         $response->assertJson([
             "message" => "Unauthenticated."
         ]);
+    }
+
+    public function test_index_when_token_valid()
+    {
+        $user = User::factory()->create();
+
+        $response = $this->actingAs($user)->get('api/users');
+        $response->assertStatus(200);
+        $response->assertJsonFragment(['message' => 'ok']);
     }
 }
