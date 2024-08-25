2d4883a6111239cf7a0a434aaa6bf7005fad4f74
dimas nurhamzah <hamzahdimas85@gmail.com>
2024-08-25 08:12:35 -0700

    create controller mass-user

--- app/Http/Controllers/Api/CreateManyUserController.php
+++ app/Http/Controllers/Api/CreateManyUserController.php
@@ -3,9 +3,17 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Jobs\ProcessStoreBulkUser;
+use Illuminate\Http\JsonResponse;
 
 class CreateManyUserController extends Controller
 {
-    //
+    public function __invoke(): JsonResponse
+    {
+        ProcessStoreBulkUser::dispatch();
+
+        return response()->json([
+            'message' => 'success store mass user'
+        ]);
+    }
 }
