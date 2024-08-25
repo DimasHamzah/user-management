b0d8703ae8127add91088ea5a67ac910610d852d
dimas nurhamzah <hamzahdimas85@gmail.com>
2024-08-25 07:00:37 -0700

    create test loginRequest valid and rules

--- tests/Feature/Validation/LoginRequestTest.php
+++ tests/Feature/Validation/LoginRequestTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature\Validation;
 
 use App\Http\Requests\Api\LoginRequest;
+use Illuminate\Support\Facades\Validator;
 use Tests\TestCase;
 
 class LoginRequestTest extends TestCase
@@ -13,4 +14,32 @@ class LoginRequestTest extends TestCase
 
         $this->assertTrue($request->authorize());
     }
+
+    public function test_rules()
+    {
+        $request = new LoginRequest();
+        $rules = $request->rules();
+
+        $this->assertArrayHasKey('email', $rules);
+        $this->assertArrayHasKey('password', $rules);
+
+        $this->assertEquals('required|email', $rules['email']);
+        $this->assertEquals('required', $rules['password']);
+    }
+
+    public function test_valid_request()
+    {
+        $data = [
+            'email' => 'test@example.com',
+            'password' => 'password123'
+        ];
+
+        $request = new LoginRequest();
+        $request->replace($data);
+
+        $validator = Validator::make($request->all(), $request->rules());
+        $this->assertFalse($validator->fails());
+    }
+
+
 }
