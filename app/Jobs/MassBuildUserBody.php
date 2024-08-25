3a84c5a75102724417e4b1ffc0de637cf757491e
dimas nurhamzah <hamzahdimas85@gmail.com>
2024-08-25 09:31:31 -0700

    finish job

---
+++ app/Jobs/MassBuildUserBody.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\User;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Hash;
+
+class MassBuildUserBody implements ShouldQueue
+{
+    use Queueable, Dispatchable, InteractsWithQueue, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct(public array $users)
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        foreach ($this->users as $user) {
+            User::create([
+                'name' => $user['name'],
+                'email' => $user['email'],
+                'password' => Hash::make($user['password']),
+            ]);
+        }
+    }
+}
---
+++ tests/Feature/Job/MassBulkUserBodyTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature\Job;
+
+use App\Jobs\MassBuildUserBody;
+use Illuminate\Support\Facades\Queue;
+use Tests\TestCase;
+use Faker\Factory as Faker;
+
+
+class MassBulkUserBodyTest extends TestCase
+{
+    public function test_job_dispatch_body()
+    {
+        Queue::fake();
+
+        $faker = Faker::create();
+
+        $users = [];
+        for ($i = 0; $i < 1000; $i++) {
+            $users[] = [
+                'name' => $faker->name,
+                'email' => $faker->unique()->safeEmail,
+                'password' => 'password123',
+            ];
+        }
+        MassBuildUserBody::dispatch($users);
+
+
+        Queue::assertPushed(MassBuildUserBody::class);
+    }
+}
