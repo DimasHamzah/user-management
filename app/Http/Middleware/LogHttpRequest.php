6baf2bf9d0bec1b4d0d45483ea384b547f81cba4
dimas nurhamzah <hamzahdimas85@gmail.com>
2024-08-25 02:47:34 -0700

    create middleware for LogHttpRequest

--- app/Http/Middleware/LogHttpRequest.php
+++ app/Http/Middleware/LogHttpRequest.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Symfony\Component\HttpFoundation\Response;
 
 class LogHttpRequest
@@ -15,6 +16,30 @@ class LogHttpRequest
      */
     public function handle(Request $request, Closure $next): Response
     {
-        return $next($request);
+        $startTime = microtime(true);
+
+        Log::info('request', [
+            'timestamp' => now()->toDateTimeString(),
+            'method' => $request->method(),
+            'uri' => $request->fullUrl(),
+            'headers' => $request->headers->all(),
+            'body' => $request->all(),
+            'ip_address' => $request->ip(),
+            'user_id' => auth()->check() ? auth()->id() : null
+        ]);
+
+        $response = $next($request);
+
+        $endTime = microtime(true);
+        $duration = $endTime - $startTime;
+
+        Log::info('response', [
+            'timestamp' => now()->toDateTimeString(),
+            'status' => $response->getStatusCode(),
+            'headers' => $response->headers->all(),
+            'duration' => $duration
+        ]);
+
+        return $response;
     }
 }
