300943dcab3745c3d9e3e81c54706a14ffa5542c
dimas nurhamzah <hamzahdimas85@gmail.com>
2024-08-25 07:39:18 -0700

    use controller user to routes api

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthenticationController;
 use App\Http\Controllers\Api\RegisterController;
+use App\Http\Controllers\Api\UserManagementController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -12,4 +13,8 @@ Route::get('/user', function (Request $request) {
 Route::middleware('log-http')->group(function () {
    Route::post('auth/login', AuthenticationController::class);
    Route::post('register', RegisterController::class);
+});
+
+Route::middleware(['auth:sanctum'])->group(function () {
+   Route::get('/users', [UserManagementController::class, 'index']);
 });
