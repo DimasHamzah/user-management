5915bc3f4f726c718bf0eedfd1827919364b8e9e
dimas nurhamzah <hamzahdimas85@gmail.com>
2024-08-25 07:15:20 -0700

    test valid and not valid

--- app/Http/Controllers/Api/AuthenticationController.php
+++ app/Http/Controllers/Api/AuthenticationController.php
@@ -4,12 +4,30 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\LoginRequest;
+use App\Models\User;
+use Illuminate\Http\Exceptions\HttpResponseException;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthenticationController extends Controller
 {
     public function __invoke(LoginRequest $request)
     {
         $validated = $request->validated();
+
+        $user = User::where('email', $validated['email'])->first();
+
+        if (!$user || !Hash::check($validated['password'], $user->password)) {
+            throw new HttpResponseException(response([
+                'message' => 'These credentials do not match our records.'
+            ], 422));
+        }
+
+        $token = $user->createToken($user->name)->plainTextToken;
+
+        return response()->json([
+            'message' => 'Welcome to my app',
+            'data' => $token
+        ]);
     }
 }
--- tests/Feature/Api/AuthenticationControllerTest.php
+++ tests/Feature/Api/AuthenticationControllerTest.php
@@ -2,8 +2,8 @@
 
 namespace Tests\Feature\Api;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 use Tests\TestCase;
 
 class AuthenticationControllerTest extends TestCase
@@ -23,5 +23,60 @@ class AuthenticationControllerTest extends TestCase
             'email' => ['The email field must be a valid email address.'],
             'password' => ['The password field is required.']
         ]);
+    }
+
+    public function test_login_when_data_empty()
+    {
+
+        $response = $this->post('/api/auth/login', []);
+
+        $response->assertStatus(422);
+        $response->assertJsonValidationErrors(['email','password']);
+        $response->assertJsonFragment([
+            'email' => ['The email field is required.'],
+            'password' => ['The password field is required.']
+        ]);
+    }
+
+    public function test_login_when_email_and_password_not_valid()
+    {
+        User::factory()->create([
+            'password' => Hash::make('Password')
+        ]);
+
+        $data = [
+            'email' => 'test!asdasd@gmail.com',
+            'password' => 'Password',
+        ];
+
+        $response = $this->post('/api/auth/login', $data);
+        $response->assertStatus(422);
+        $response->assertJson([
+            'message' => 'These credentials do not match our records.',
+        ]);
+    }
+
+    public function test_login_success()
+    {
+        $user = User::factory()->create([
+            'password' => Hash::make('Password')
+        ]);
+
+        $data = [
+            'email' => $user->email,
+            'password' => 'Password',
+        ];
+
+        $response = $this->post('/api/auth/login', $data);
+
+        $response->assertStatus(200);
+        $response->assertJson([
+            'message' => 'Welcome to my app',
+            'data' => true
+        ]);
+
+        $responseData = $response->json();
+        $this->assertIsString($responseData['data']);
+        $this->assertNotEmpty($responseData['data']);
     }
 }
