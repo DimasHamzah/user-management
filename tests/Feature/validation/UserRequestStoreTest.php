1f620568bd23618c66a1611e721ef3429c3f80b9
dimas nurhamzah <hamzahdimas85@gmail.com>
2024-08-25 03:27:47 -0700

    create test rules validation UserRuquestStore

--- tests/Feature/validation/UserRequestStoreTest.php
+++ tests/Feature/validation/UserRequestStoreTest.php
@@ -2,8 +2,7 @@
 
 namespace Tests\Feature\validation;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
+use App\Http\Requests\UserRequestStore;
 use Tests\TestCase;
 
 class UserRequestStoreTest extends TestCase
@@ -13,5 +12,20 @@ class UserRequestStoreTest extends TestCase
         $request = new UserRequestStore();
 
         $this->assertTrue($request->authorize());
+    }
+
+    public function test_rules()
+    {
+        $request = new UserRequestStore();
+
+        $rules = $request->rules();
+
+        $this->assertArrayHasKey('name', $rules);
+        $this->assertArrayHasKey('email', $rules);
+        $this->assertArrayHasKey('password', $rules);
+
+        $this->assertEquals('required|string', $rules['name']);
+        $this->assertEquals('required|string|email:rfc,dns|unique:users,email', $rules['email']);
+        $this->assertEquals('required|string', $rules['password']);
     }
 }
